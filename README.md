45e2c071815c127d904ec84586e275a81b46d9c6
DongUk-Shin <az9170@naver.com>
2023-10-26 22:06:30 +0900

    로그인 로그아웃 구현

--- README.md
+++ README.md
@@ -4,3 +4,11 @@
 set FLASK_APP=kickboard
 
 __init.py__ 파일 실행
+
+설치 모듈
+Flask    
+Flask-Migrate
+Flask-SQLAlchemy 
+Flask-Login
+Flask-Session
+
--- kickboard/__init__.py
+++ kickboard/__init__.py
@@ -1,6 +1,7 @@
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, flash, redirect, render_template, request, url_for, session
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 import config
 
 #객체 생성
@@ -9,11 +10,12 @@ migrate = Migrate()
 
 app = Flask(__name__)
 app.config.from_object(config)
+app.secret_key = 'abcde'
 
 db.init_app(app)
 migrate.init_app(app, db)
 
-
+from .models import information
 
 @app.route('/')
 def default():
@@ -21,7 +23,8 @@ def default():
 
 @app.route('/main')
 def main():
-    return render_template("main.html")
+    session_id = session.get('session_user')
+    return render_template("main.html", session_id=session_id)
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -32,13 +35,34 @@ def signup():
         birth = request.form['birth']
         call = request.form['call']
 
-        from .models import information
-        user = information(user_id=user_id, password=password, name=name, birth=birth, call=call)
+        hashed_password = generate_password_hash(password)  # 비밀번호 해싱
+
+        user = information(user_id=user_id, password=hashed_password, name=name, birth=birth, call=call)
+
         db.session.add(user)
         db.session.commit()
-        return redirect(url_for('login'))
+        return redirect(url_for('main'))
 
     return render_template('signup.html')
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    if request.method == 'POST':
+        user_id_input = request.form['user_id_input']
+        password_input = request.form['password_input']
+        user = information.query.filter_by(user_id=user_id_input).first()
+        
+        if user and check_password_hash(user.password, password_input):
+            session.clear()
+            session['session_user'] = user.user_id #세션에 id 저장
+            return redirect(url_for('main'))
+        
+    return render_template('signin.html')
+
+@app.route('/logout/')
+def logout():
+    session.clear()
+    return redirect(url_for('main'))
 
 
 if __name__ == '__main__':
