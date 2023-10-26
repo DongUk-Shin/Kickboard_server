a45eb16e3c111bdc00d5e08eaec01021539182bf
DongUk-Shin <az9170@naver.com>
2023-10-26 16:46:29 +0900

    모델 생성

--- kickboard/__init__.py
+++ kickboard/__init__.py
@@ -10,7 +10,6 @@ migrate = Migrate()
 app = Flask(__name__)
 app.config.from_object(config)
 
-
 db.init_app(app)
 migrate.init_app(app, db)
 
@@ -19,5 +18,11 @@ def main_page():
     return render_template("main.html")
 
 
+@app.route('/login/')
+def login():
+    return render_template("main.html")
+
+
 if __name__ == '__main__':
+    #app.run(host="220.69.208.119", port=8000, debug=True)
     app.run(port=8000, debug=True)
